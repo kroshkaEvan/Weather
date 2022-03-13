2bd0689c94f23ed37f019eec99b9dbbc6e10fcc9
Эван Крошкин <>
2022-03-13 05:08:14 +0300

    Connected cocoapods (pod 'lottie-ios') from https://github.com/airbnb/lottie-ios

--- Weather/Weather/Views/Animation/Animation.swift
+++ Weather/Weather/Views/Animation/Animation.swift
@@ -5,4 +5,28 @@
 //  Created by Эван Крошкин on 13.03.22.
 //
 
-import Foundation
+import SwiftUI
+import Lottie
+
+struct CustomAnimationView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<CustomAnimationView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<CustomAnimationView>) {
+        uiView.subviews.forEach({$0.removeFromSuperview()})
+        let animationView = AnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor).isActive = true
+        animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor).isActive = true
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+    }
+}
